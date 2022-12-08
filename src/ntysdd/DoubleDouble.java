418f6a7abbe5783a6663d0828d1d3c9aef2cc227
ntysdd <ntysdd@gmail.com>
2022-12-08 20:17:00 +0800

    Create DoubleDouble.java

--- src/ntysdd/DoubleDouble.java
+++ src/ntysdd/DoubleDouble.java
@@ -635,6 +635,20 @@ public final strictfp class DoubleDouble {
         return eps.add(eps2).add(f0);
     }
 
+    public static DoubleDouble cbrt(DoubleDouble value) {
+        if (value.second == 0) {
+            return cbrt(value.first);
+        }
+        double f0 = Math.cbrt(value.first);
+        if (!(Double.isFinite(f0))) {
+            return DoubleDouble.valueOf(f0);
+        }
+        DoubleDouble h = mul(f0, f0).mul(-f0).add(value).div(value);
+        DoubleDouble eps = h.mul(f0).div(3);
+        double eps2 = f0 * h.first * h.first * (2.0 / 9.0);
+        return eps.add(eps2).add(f0);
+    }
+
     public static DoubleDouble cbrt(double value) {
         if (value == 0) {
             if (Double.doubleToRawLongBits(value) == 0) {
